0d2f2aaa43c4fd4404518f1d5061352f32e7f617
Kelvin Ma <kelvin@kelvin.ma>
2021-04-12 22:11:52 -0700

    Request permission in order to query content

--- app/src/main/java/com/learntodroid/wallpaperapptutorial/WallpaperActivity.java
+++ app/src/main/java/com/learntodroid/wallpaperapptutorial/WallpaperActivity.java
@@ -9,5 +9,7 @@ public class WallpaperActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallpaper);
+
+
     }
 }
--- app/src/main/java/com/learntodroid/wallpaperapptutorial/WallpapersListFragment.java
+++ app/src/main/java/com/learntodroid/wallpaperapptutorial/WallpapersListFragment.java
@@ -1,33 +1,45 @@
 package com.learntodroid.wallpaperapptutorial;
 
+import android.Manifest;
 import android.app.WallpaperManager;
-import android.content.ComponentName;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.transition.Transition;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WallpapersListFragment extends Fragment implements WallpaperSelectListener {
+    private static  final int READ_EXTERNAL_STORAGE_PERMISSION_REQUEST = 1;
+
     private RecyclerView wallpaperRecyclerView;
     private WallpaperGalleryRecyclerAdapter wallpaperGalleryRecyclerAdapter;
     private List<Wallpaper> wallpapers;
@@ -36,6 +48,14 @@ public class WallpapersListFragment extends Fragment implements WallpaperSelectL
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (!checkPermissions()) {
+            return;
+        }
+
+        initUI();
+    }
+
+    private void initUI() {
         wallpapers = new ArrayList<>();
         wallpapers.add(new Wallpaper("Beach", "https://images.pexels.com/photos/853199/pexels-photo-853199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"));
         wallpapers.add(new Wallpaper("Mountain", "https://images.pexels.com/photos/1261728/pexels-photo-1261728.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"));
@@ -47,6 +67,23 @@ public class WallpapersListFragment extends Fragment implements WallpaperSelectL
         wallpaperGalleryRecyclerAdapter.setWallpapers(wallpapers);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == READ_EXTERNAL_STORAGE_PERMISSION_REQUEST) {
+            initUI();
+        }
+    }
+
+    private boolean checkPermissions() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            if (!(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
+                requestPermissions((new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}), READ_EXTERNAL_STORAGE_PERMISSION_REQUEST);
+                return false;
+            }
+            return true;
+        }
+        return true;
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -108,19 +145,57 @@ public class WallpapersListFragment extends Fragment implements WallpaperSelectL
     }
 
     @Override
-    public void onWallpaperSelect(Wallpaper wallpaper) {
+    public void onWallpaperSelect(final Wallpaper wallpaper) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    final WallpaperManager wallpaperManager = WallpaperManager.getInstance(getContext());
+                        query();
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+//                                Intent intent = wallpaperManager.getCropAndSetWallpaperIntent(Uri.parse("content://media/external/images/media/1"));
+//                                startActivity(intent);
+                            }
+                        });
+                }
+            });
+        }
+    }
 
-        Glide.with(getContext())
-                .asBitmap()
-                .load(wallpaper.getImageUri())
-                .into(new SimpleTarget<Bitmap>() {
-                    @Override
-                    public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
-                        setHomeScreenWallpaper(resource);
-//                        setCroppedHomeScreenWallpaper(resource);
-//                        setLockScreenWallpaper(resource);
-//                        Toast.makeText(getContext(), wallpaper.getTitle() + " selected", Toast.LENGTH_SHORT).show();
-                    }
-                });
+    private void query() {
+        Uri collection =  MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = new String[] {
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.TITLE,
+        };
+        try (Cursor cursor = getActivity().getApplicationContext().getContentResolver().query(
+                collection,
+                projection,
+                null,
+                null,
+                 null
+        )) {
+            // Cache column indices.
+            int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID);
+            int nameColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME);
+            int titleColumnn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.TITLE);
+            System.out.println("KELVIN cursor=" + cursor.getCount());
+            while (cursor.moveToNext()) {
+                // Get values of columns for a given video.
+                long id = cursor.getLong(idColumn);
+                String name = cursor.getString(nameColumn);
+                String title = cursor.getString(titleColumnn);
+
+                Uri contentUri = ContentUris.withAppendedId(
+                        MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id);
+                System.out.println("KELVIN uri=" + contentUri + " title=" + title + " name = " + name);
+            }
+        }
     }
 }
